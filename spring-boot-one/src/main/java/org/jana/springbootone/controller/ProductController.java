422e4109a5b6d3f0fa59ea3dc247fea28476138a
janarthan <janarthan@finnovation.lk>
2018-08-08 15:55:12 +0530

    Update

--- spring-boot-one/src/main/java/org/jana/springbootone/controller/ProductController.java
+++ spring-boot-one/src/main/java/org/jana/springbootone/controller/ProductController.java
@@ -1,7 +1,9 @@
 package org.jana.springbootone.controller;
 
+import org.jana.springbootone.model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +19,10 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public ResponseEntity saveProduct() {
-        System.out.println("Save product");
+    public ResponseEntity saveProduct(@RequestBody Product product) {
+        System.out.println("Save product - " + product);
         //cal service
-        return new ResponseEntity("Product saved succesfully", HttpStatus.OK);
+        return new ResponseEntity("Product saved successfully", HttpStatus.OK);
     }
 
 }
