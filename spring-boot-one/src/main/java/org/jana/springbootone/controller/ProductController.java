b8c5c378265b75044afd16bbaa72576be4ab898a
janarthan <janarthan@finnovation.lk>
2018-08-08 16:55:24 +0530

    Add delete api

--- spring-boot-one/src/main/java/org/jana/springbootone/controller/ProductController.java
+++ spring-boot-one/src/main/java/org/jana/springbootone/controller/ProductController.java
@@ -3,10 +3,7 @@ package org.jana.springbootone.controller;
 import org.jana.springbootone.model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -29,6 +26,13 @@ public class ProductController {
         System.out.println("Save product - " + product);
         //cal service
         return new ResponseEntity("Product saved successfully", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity delete(@PathVariable Integer id) {
+        System.out.println("Delete product - " + id);
+        //cal service
+        return new ResponseEntity("Deleted " + id, HttpStatus.OK);
     }
 
 }
