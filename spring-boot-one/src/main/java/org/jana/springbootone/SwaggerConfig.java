8898ff3fb2fa2c3cd05c1f1596cbd86099d2d60e
janarthan <janarthan@finnovation.lk>
2018-08-08 16:53:24 +0530

    Update swagger config

--- spring-boot-one/src/main/java/org/jana/springbootone/SwaggerConfig.java
+++ spring-boot-one/src/main/java/org/jana/springbootone/SwaggerConfig.java
@@ -3,6 +3,8 @@ package org.jana.springbootone;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,7 +16,7 @@ import com.google.common.base.Predicate;
 public class SwaggerConfig {
 
     Predicate<String> predicate = s -> {
-        return s.equals("/product/add");
+        return s.equals("/product/*");
     };
 
     @Bean
@@ -24,6 +26,18 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("org.jana.springbootone.controller"))
                 .paths(predicate)
                 .build();
+    }
+
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot REST API",
+                "Spring Boot REST API for Online Store",
+                "1.0",
+                "Terms of service",
+                new Contact("Jana Rathan", "blogOfAnotherDeveloper.blogspot.com", "a@b.c"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0");
+        return apiInfo;
     }
 
 }
